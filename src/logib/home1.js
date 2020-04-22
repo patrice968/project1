bbf8a12f707486b560f3d7eef1ceb9d575c4e8c6
patrice968 <62956914+patrice968@users.noreply.github.com>
2020-04-22 16:49:40 -0400

    Create home1.js

---
+++ src/logib/home1.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+  TouchableHighlight,
+  ImageBackground,Platform,Animated
+} from 'react-native';
+
+
+
+class Home extends React.Component {
+
+  
+
+  render() {
+    return (
+
+
+      <View style={styles.house1Container}>
+
+       
+        <View style={styles.touchableButton2}>
+        <Text style={styles.buttonText}
+ Enter ZIP Code: 
+ </Text>
+ </View>
+
+ View style={styles.touchableButton3}>
+        <Text style={styles.buttonText}
+ Radius: 
+ </Text>
+ </View>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  house1ontainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  touchableButton1: {
+        backgroundColor: 'blue',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  touchableButton2: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  touchableButton3: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  buttonText: {
+     
+        color: 'black',
+        fontSize: 14,
+        fontWeight:'bold',
+  },
+
+});
+
+
+export default Home;
