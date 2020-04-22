fd5cdab12d097776e984c313e79e18e45eac55f4
patrice968 <62956914+patrice968@users.noreply.github.com>
2020-04-22 16:47:14 -0400

    Create homescreen.js

---
+++ src/login/homescreen.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+  TouchableHighlight,
+  ImageBackground,Platform,Animated
+} from 'react-native';
+
+
+
+class Homescreen extends React.Component {
+
+  
+
+  render() {
+    return (
+
+
+      <View style={styles.container}>
+       <Image source ={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSn0uQIyp8rEsqMYatm6_YJkc4o7NzbPEvfj9p_mCpeFQObvxkF&usqp=CAU'}}
+ style={{ height: 300, width: 300, marginTop: -10, }}
+ ></Image>
+        <TouchableHighlight>
+ <View style={styles.touchableButton1}>
+                        <Text style={styles.buttonText}>
+                            Continue
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+
+ 
+
+ 
+
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+
+
+  textContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
+  text: {
+    
+    fontSize: 8,
+    fontWeight:'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+   touchableButton1: {
+        backgroundColor: 'blue',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+   
+   
+    },
+   
+    buttonText: {
+     
+        
+        fontSize: 14,
+        fontWeight:'bold',
+    
+    }
+});
+
+
+export default Homescreen;
