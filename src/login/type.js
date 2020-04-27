c79751b451b64c86916d01b4eb0e8545c3319f70
patrice968 <62956914+patrice968@users.noreply.github.com>
2020-04-27 12:55:09 -0400

    Create type.js

---
+++ src/login/type.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+  TouchableHighlight,
+  ImageBackground,Platform,Animated
+} from 'react-native';
+
+
+
+class Type extends React.Component {
+
+  
+
+  render() {
+    return (
+
+
+      <View style={styles.container}>
+         <TouchableHighlight>
+ <View style={styles.button1}>
+                        <Text style={styles.buttonText}>
+                            One Family House
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+        <TouchableHighlight>
+ <View style={styles.button2}>
+                        <Text style={styles.buttonText}>
+                            Two Family House
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+
+<TouchableHighlight>
+ <View style={styles.button3}>
+                        <Text style={styles.buttonText}>
+                            Condo
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+
+<TouchableHighlight>
+ <View style={styles.button4}>
+                        <Text style={styles.buttonText}>
+                            Apartment 
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+button1: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  button2: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  button3: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  },
+  button4: {
+        backgroundColor: 'green',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+  }, 
+  buttonText: {
+     
+        color: 'black',
+        fontSize: 14,
+        fontWeight:'bold',
+  },
+});
+
+
+export default Type;
