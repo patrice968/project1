503997c3025e2828780ad38ca3ba9d53af417bbd
patrice968 <62956914+patrice968@users.noreply.github.com>
2020-04-27 12:52:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ import { createStackNavigator } from 'react-navigation';
 import Homescreen from './src/login/homescreen.js';
 import Email from './src/login/email.js';
 import File from './src/login/newfile.js';
-import Home from './src/login/newfile.js';
-
-
+import Home from './src/login/home1.js';
+import Home2 from './src/login/home2.js';
+import Type from './src/login/newfile.js';
 
 
 
@@ -16,6 +16,8 @@ const RootStack = createStackNavigator(
   Email: {screen: Email},
   File: {screen: File},
   Home1:{screen: Home},
+  Home2:{screen: Home2}, 
+  Type:{screen: Type},
 },
 {
     initialRouteName: 'Home',
