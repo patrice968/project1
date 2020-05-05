c49d12b5f64469a039f371c6538066ff4064314a
patrice968 <62956914+patrice968@users.noreply.github.com>
2020-05-05 14:50:45 -0400

    Create Page5.js

---
+++ src/login/Page5.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+  Button,
+  Image,
+  TouchableHighlight,
+  ImageBackground,Platform,Animated
+} from 'react-native';
+
+
+
+class Page5 extends React.Component {
+
+  
+
+  render() {
+    return (
+
+
+      <View style={styles.container}>
+       <Image source ={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvhQ_MMSqF-udXDQFF4nGGUMud6c6u2NkTgKHO-I6-Ml7vCE_-&usqp=CAU'}}
+ style={{ height: 300, width: 300, marginTop: -10, }}
+ ></Image>
+<Text style={styles.text}>
+Excellent Choice
+</Text>
+        <TouchableHighlight
+onPress={() => this.props.navigation.navigate('Homescreen')}
+>
+<View style={styles.touchableButton1}>
+<Text style={styles.buttonText}>
+Continue
+</Text>
+</View>
+</TouchableHighlight>
+
+ </View>
+
+ 
+
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+
+
+  
+
+
+  text: {
+    
+    fontSize: 25,
+    fontWeight:'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+   touchableButton1: {
+        backgroundColor: 'blue',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+         borderRadius: 10,
+   
+   
+    },
+   
+    buttonText: {
+     
+        
+        fontSize: 14,
+        fontWeight:'bold',
+    
+    }
+});
+
+
+export default Page5;
